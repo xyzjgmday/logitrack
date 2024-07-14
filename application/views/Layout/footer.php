7a4a8bb2952af1cf50935b3bee197e166e74d938
riyanada <riyanmaulana402@yahoo.co.id>
2024-07-15 00:32:55 +0700

    add versioning

--- application/views/Layout/footer.php
+++ application/views/Layout/footer.php
@@ -3,7 +3,18 @@
         <div class="m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop">
             <div class="m-stack__item m-stack__item--left m-stack__item--middle m-stack__item--last">
                 <span class="m-footer__copyright">
-                    Copyright © 2024 PT.Digital Media Utama (DIGIMU) All rights reserved.
+                    Copyright © 2024 ., <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="stroke-1.5 w-4 h-4 mr-2">
+                        <path
+                            d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z">
+                        </path>
+                        <path
+                            d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z">
+                        </path>
+                        <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0"></path>
+                        <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5"></path>
+                    </svg>v.0.0.3 &mdash; PT.Digital Media Utama (DIGIMU) All rights reserved.
                 </span>
             </div>
         </div>
